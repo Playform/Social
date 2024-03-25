df3b92f55e6bef2239d86ede8230e92e4df5d829
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-26 01:40:20 +0200

    squash!

--- sherlock/sherlock.py
+++ sherlock/sherlock.py
@@ -651,7 +651,7 @@ def main():
     # Check for newer version of Sherlock. If it exists, let the user know about it
     try:
         r = requests.get(
-            "https://raw.githubusercontent.com/sherlock-project/sherlock/master/sherlock/sherlock.py"
+            "HTTPS://Raw.GitHubUserContent.Com/sherlock-project/sherlock/master/sherlock/sherlock.py"
         )
 
         remote_version = str(re.findall('__version__ = "(.*)"', r.text)[0])
